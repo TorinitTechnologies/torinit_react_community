9865cb76e4ffc012c2ed02d2a73420cefb92b3dc
palakghai31 <palak.ghai@torinit.ca>
2021-07-19 20:49:50 +0530

    more changes

--- src/ImagePlayer/ImagePlayer.js
+++ src/ImagePlayer/ImagePlayer.js
@@ -1,48 +1,110 @@
-import React from 'react';
-import './ImagePlayer.css';
+import React,{useEffect} from "react";
+import "./ImagePlayer.css";
 
-export const ImagePlayer = ({images,timeGap}=props) => {
-    const [current,setCurrent] = React.useState(0);
-    const [isPlaying,setPlaying] = React.useState(false);
+export const ImagePlayer = ({
+  images,
+  timeGap,
+  autoplay,
+  backdrop,
+  showDots,
+} = props) => {
+  const [current, setCurrent] = React.useState(0);
+  const [isPlaying, setPlaying] = React.useState(false);
 
-    const handlePrev=()=>{
-        if(current!==0){
-            setCurrent(current-1)
-        }
+  const handlePrev = () => {
+    if (current !== 0) {
+      setCurrent(current - 1);
     }
+  };
 
-    const handleNext=()=>{
-        if(current<images.length-1){
-            setCurrent(current+1)
-        }
-        else setCurrent(0)
-    }
+  const handleNext = () => {
+    if (current < images.length - 1) {
+      setCurrent(current + 1);
+    } else setCurrent(0);
+  };
 
-    if(isPlaying){
-        setTimeout(()=>{
-            handleNext();
-        },timeGap || 2000);
-    }
+  if (isPlaying) {
+    setTimeout(() => {
+        handleNext();
+    }, timeGap || 3000);
+  }
+  
 
-    return(
-        <div style={
-            {display:'flex',
-            flexDirection:'column',
-            border:'1px solid',
-            borderRadius:'10px',
-            padding:'40px 10px',
-            height:'200px',
-            width:'300px'}
-            }>
-        <div className='main-image-wrapper'>
-            <span className={current===0 && 'inactive'} onClick={handlePrev}>{'<'} </span>
-            <div className='image-wrapper'>
-                <img src={images[current]} className='img-imgWrapper' alt=''/>
+  console.log()
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {backdrop && <div className="backdrop"></div>}
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            borderRadius: "10px",
+            padding: "40px 10px",
+            height: "200px",
+            width: "300px",
+            zIndex: "100000",
+            backgroundColor: "white",
+          }}
+        >
+          <div className="main-image-wrapper">
+            {!isPlaying && (
+              <span
+                className={current === 0 && "inactive"}
+                onClick={handlePrev}
+              >
+                {"<"}{" "}
+              </span>
+            )}
+            <div className="image-wrapper fade">
+              <img src={images[current]} className="img-imgWrapper" alt="" />
             </div>
-            <span className={current===images.length-1 && 'inactive'} onClick={handleNext}>{'>'}</span>
+            {!isPlaying && (
+              <span
+                className={current === images.length - 1 && "inactive"}
+                onClick={handleNext}
+              >
+                {">"}
+              </span>
+            )}
+          </div>
+          {autoplay && (
+            <>
+              {isPlaying && (
+                <span
+                  className="play-pause-button"
+                  onClick={() => {
+                      setPlaying(false)
+                  }}
+                >
+                  {"||"}
+                </span>
+              )}
+              {!isPlaying && (
+                <span
+                  className="play-pause-button"
+                  onClick={() => setPlaying(true)}
+                >
+                  {"I>"}
+                </span>
+              )}
+            </>
+          )}
         </div>
-        {isPlaying && <span className='play-pause-button' onClick={()=>setPlaying(false)}>{'||'}</span>}
-        {!isPlaying && <span  className='play-pause-button' onClick={()=>setPlaying(true)} >{'|>'}</span>}
+      </div>
+      {showDots && (
+        <div className="dot-wrapper">
+          {images.map((img,index) => {
+            return <div className={`dot ${current===index && 'active-dot'}`} onClick={()=>setCurrent(index)}></div>;
+          })}
         </div>
-    )
-}
+      )}
+    </>
+  );
+};
